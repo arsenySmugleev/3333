b5823ad64db3c45be3c4eaa37d848a544ccf85b7
Arseny <smugleevv@gmail.com>
2026-03-11 21:46:58 +0300

    устранил ошибку с цикличными импортами

--- src/models/appointment.py
+++ src/models/appointment.py
@@ -1,17 +1,20 @@
 from datetime import datetime
-
+from typing import TYPE_CHECKING
 import sqlalchemy as sa
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql.schema import ForeignKey
-from src.models.doctor import Doctor
 from src.models.base import Base
+
+
+if TYPE_CHECKING:
+    from src.models.doctor import Doctor
 
 
 class Appointment(Base):
     __tablename__ = "appointments"
     id: Mapped[int] = mapped_column(primary_key=True)
-    doc_id: Mapped[int] = mapped_column(ForeignKey("doctors.id"), ondelete="CASCADE")
-    time_start: Mapped[datetime.datetime] = mapped_column(sa.DateTime())
+    doc_id: Mapped[int] = mapped_column(ForeignKey("doctors.id"))
+    time_start: Mapped[datetime] = mapped_column(sa.DateTime())
     name: Mapped[str] = mapped_column(sa.String())
 
     doctor: Mapped["Doctor"] = relationship(back_populates="appointment")
