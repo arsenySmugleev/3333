7588c92c26b3816346b5b74bcfd1d4c74815d88d
Arseny <smugleevv@gmail.com>
2026-03-16 22:03:48 +0300

    добавил appointment круд

---
+++ src/handlers/appointment.py
@@ -0,0 +1,77 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import selectinload
+
+from src.db import get_session
+from src.schemas.appointment import Appointment, AppointmentCreate, AppointmentUpdate
+from src.models.appointment import Appointment as AppointmentModel
+
+
+router = APIRouter(prefix="/appointment", tags=["appointment"])
+
+
+async def get_db_session() -> AsyncSession:
+    async with get_session() as session:
+        yield session
+
+
+@router.get("/{id}", response_model=Appointment)
+async def get_appointment(
+        id: int,
+        session: AsyncSession = Depends(get_db_session)
+) -> Appointment:
+    stmt = select(AppointmentModel).where(AppointmentModel.id == id).options(selectinload(AppointmentModel.doctor))
+    result = await (session.execute(stmt))
+    appointment = result.scalar_one_or_none()
+    if not appointment:
+        raise HTTPException(status_code=404, detail="Appointment not found")
+    return appointment
+
+
+@router.post("/", response_model=Appointment)
+async def create_appointment(
+        appointment_data: AppointmentCreate,
+        session: AsyncSession = Depends(get_db_session)
+):
+    db_appointment = AppointmentModel(**appointment_data.model_dump())
+    session.add(db_appointment)
+    await session.commit()
+    stmt = (select(AppointmentModel).where(AppointmentModel.id == db_appointment.id)
+            .options(selectinload(AppointmentModel.doctor)))
+    result = await session.execute(stmt)
+    db_appointment = result.scalar_one_or_none()
+    return db_appointment
+
+
+@router.patch("/{id}", response_model=Appointment)
+async def update_appointment(
+        id: int,
+        appointment_data: AppointmentUpdate,
+        session: AsyncSession = Depends(get_db_session)
+):
+    stmt = select(AppointmentModel).where(AppointmentModel.id == id).options(selectinload(AppointmentModel.doctor))
+    result = await session.execute(stmt)
+    appointment_db = result.scalar_one_or_none()
+    if not appointment_db:
+        raise HTTPException(status_code=404, detail="Appointment not found")
+    update_appointment_data = appointment_data.model_dump(exclude_unset=True)
+    for key, value in update_appointment_data.items():
+        setattr(appointment_db, key, value)
+    await session.commit()
+    await session.refresh(appointment_db)
+    return appointment_db
+
+
+@router.delete("/{id}", response_model=Appointment)
+async def delete_appointment(
+        id: int,
+        session: AsyncSession = Depends(get_db_session)
+):
+    result = await session.execute(select(AppointmentModel).where(AppointmentModel.id == id))
+    appointment_db = result.scalar_one_or_none()
+    if not appointment_db:
+        raise HTTPException(status_code=404, detail="Appointment not found")
+    await session.delete(appointment_db)
+    await session.commit()
+    return appointment_db
