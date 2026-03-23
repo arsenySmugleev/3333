f4a26ce1a0212e3d903281a3518ecd42aada224e
Arseny <smugleevv@gmail.com>
2026-03-23 21:50:38 +0300

    добавил тайпчекинг, чтобы не было циклического импорта

--- src/models/insurance.py
+++ src/models/insurance.py
@@ -1,13 +1,17 @@
-from sqlalchemy.sql.schema import ForeignKey
+from typing import TYPE_CHECKING
 
-from src.models.med_card import MedCard
+from sqlalchemy.sql.schema import ForeignKey
 from src.models.base import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+if TYPE_CHECKING:
+    from src.models.med_card import MedCard
 
 
 class Insurance(Base):
     __tablename__ = "insurances"
     id: Mapped[int] = mapped_column(primary_key=True)
-    med_card_id: Mapped[int] = mapped_column(ForeignKey('med_cards.id'), ondelete="CASCADE")
+    med_card_id: Mapped[int] = mapped_column(ForeignKey("med_cards.id"))
 
-    med_card: Mapped["MedCard"] = relationship(back_populates="insurance")
+    med_card: Mapped["MedCard"] = relationship(back_populates="insurance", lazy="raise")
