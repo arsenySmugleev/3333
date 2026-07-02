f15f11b7399c79f296aa110d866ab6f19ebec5b5
Arseny <smugleevv@gmail.com>
2026-07-02 22:24:23 +0300

    UJSON -> JSON

--- src/application.py
+++ src/application.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
-from fastapi.responses import UJSONResponse
+from fastapi.responses import JSONResponse
 from starlette.middleware.cors import CORSMiddleware
 from src.controllers.v1.doctors import router as doctor_appointment
 from src.controllers.v1.medical_cards import router as med_card_insurance
 from src.controllers.v1.patients import router as patient_med_service
-from src.exceptions.exception_handler import register_exception_handlers
-from logging_config import configure_logging
-from request_id_middleware import RequestIdMiddleware
+from src.exceptions.handlers.exception_handler import register_exception_handlers
+from src.healthcheck.router import router as healthcheck_router
+from src.logging_config import configure_logging
+from src.request_id_middleware import RequestIdMiddleware
 
 
 def get_app() -> FastAPI:
@@ -15,7 +16,7 @@ def get_app() -> FastAPI:
     app = FastAPI(
         docs_url='/docs',
         openapi_url='/openapi.json',
-        default_response_class=UJSONResponse,
+        default_response_class=JSONResponse,
     )
 
     app.add_middleware(
@@ -26,6 +27,7 @@ def get_app() -> FastAPI:
         allow_headers=['*'],
     )
     app.add_middleware(RequestIdMiddleware)
+    app.include_router(healthcheck_router)
     app.include_router(doctor_appointment, prefix="/api/v1")
     app.include_router(patient_med_service, prefix="/api/v1")
     app.include_router(med_card_insurance, prefix="/api/v1")
